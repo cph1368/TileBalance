e7c2e8879407504e8b7d1fba54095c8fafef2511
cph1368 <31093206+cph1368@users.noreply.github.com>
2021-08-10 00:49:05 +1000

    Initial Commit

---
+++ TileBalance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TileBalance
+//
+//  Created by Corry Handayani on 10/8/21.
+//  Copyright © 2021 Corry Handayani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
