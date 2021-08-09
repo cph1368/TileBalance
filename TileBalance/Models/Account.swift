603a9b5a4c664ffa905b909c106ed0cd0df86391
cph1368 <31093206+cph1368@users.noreply.github.com>
2021-08-10 01:23:11 +1000

    implementing LOCAL JSON success

--- TileBalance/Models/Account.swift
+++ TileBalance/Models/Account.swift
@@ -4,9 +4,16 @@
 //
 //  Created by Corry Handayani on 10/8/21.
 //  Copyright © 2021 Corry Handayani. All rights reserved.
-//
+// using swift file not view file 
+ 
+
+//challenges - XCODE v11 and editor seems to have more bugs compared to latest version
 
 import Foundation
+
+//need to import swift ui in swift file, otherwise wont work
+import SwiftUI
+
 //need to include : Hashable, Codable, Identifiable{
 struct Account: Hashable, Codable, Identifiable{
     var id: Int
