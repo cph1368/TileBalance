37e0e4ea42065edffca18c39cc8c4dd9035a2d1f
cph1368 <31093206+cph1368@users.noreply.github.com>
2021-08-12 02:01:02 +1000

    adding ListView Homepage

--- TileBalance/ContentView.swift
+++ TileBalance/ContentView.swift
@@ -11,15 +11,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      ContentView()
+        VStack {
+            NavigationView {
+                ScrollView {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .frame(height: 1000)
+                            .padding()
+                        List {
+                            NavigationLink(destination: TileView()) {
+                                Text("Westpac Balance Tile")
+                            }
+                            NavigationLink(destination: RequestDetailView()) {
+                                Text("Provide Info Screen")
+                            }
+                        }
+                    }
+                    .navigationBarTitle("Components")
+                    
+                }
+                .padding()
+                
+            }
+                
+            .padding()
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ContentView()
-            
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            Group {
+                ContentView()
+                
+            }
         }
     }
 }
