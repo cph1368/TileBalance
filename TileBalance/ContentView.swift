296f5bcf786e9c9eeef5f206cd70e986b6877f61
cph1368 <31093206+cph1368@users.noreply.github.com>
2021-08-12 17:16:56 +1000

    Revert "add list navigation"
    This reverts commit 5d38887fe44a7b2a1ad74c3c96277e18e2a42982.

--- TileBalance/ContentView.swift
+++ TileBalance/ContentView.swift
@@ -11,35 +11,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    
         VStack {
-         
-            VStack {
-                Text("Component #1").padding(0)
-                    .frame(maxWidth: .infinity, alignment: .leading )
-                    .font(.footnote)
-                    .padding(.horizontal)
-                VStack(spacing: 10) {
-                    MenuRow()
-                    divider
-                    MenuRow()
+            NavigationView {
+                ScrollView {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .frame(height: 1000)
+                            .padding()
+                        List {
+                            NavigationLink(destination: TileView()) {
+                                Text("Westpac Balance Tile")
+                            }
+                            NavigationLink(destination: RequestDetailView()) {
+                                Text("Provide Info Screen")
+                            }
+                        }
+                    }
+                    .navigationBarTitle("Components")
+                    
                 }
-                .frame(maxWidth: .infinity)
-                .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
-                .cornerRadius(10)
-                .shadow(radius:3)
-            .padding(0)
+                .padding()
+                
             }
-            .frame(maxWidth: .infinity)
-            .padding()
+                
         }
- 
-        
     }
-    var divider: some View {
-        Divider().background(Color.white.blendMode(.overlay))
-            .padding(.horizontal)
-    }
+    
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             Group {
