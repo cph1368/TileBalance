6c7969b8d38b1a099740f2a6f9393dda2c40c6d1
cph1368 <31093206+cph1368@users.noreply.github.com>
2021-09-06 15:55:38 +1000

    Add smart Widget

---
+++ SmartAppWidget/SmartAppWidget.swift
@@ -0,0 +1,107 @@
+//
+//  SmartAppWidget.swift
+//  SmartAppWidget
+//
+//  Created by Corry Handayani on 6/9/21.
+//  Copyright © 2021 Corry Handayani. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), widgetcontents: "...")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(),widgetcontents: "...")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, widgetcontents: MyDataProvider.getwidgetcontent())
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let widgetcontents : String
+}
+
+struct SmartAppWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        
+        ZStack{
+            
+            VStack(alignment: .leading,spacing: 0){
+    
+                Image("Avatar-business-trip")
+                    .resizable()
+                    .scaledToFit()
+                Text("WORKING STATUS")
+                    .font(.system(size: 10))
+
+                Text("hello")
+                    .font(.system(size: 24))
+                    .fontWeight(.semibold)
+                    .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+       
+                   
+          
+                
+            }
+            .padding(.top,16)
+        
+            
+         }
+        
+       
+    }
+}
+
+@main
+struct SmartAppWidget: Widget {
+    let kind: String = "SmartAppWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            SmartAppWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct SmartAppWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        SmartAppWidgetEntryView(entry: SimpleEntry(date: Date(), widgetcontents: "My Widget Content"))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
+
+class MyDataProvider{
+    static func getwidgetcontent() -> String{
+        
+        let strings = [
+            "1 + 1 = 2",
+            "2 + 1 = 3",
+        ]
+        
+        
+        return strings.randomElement()!
+    }
+}
